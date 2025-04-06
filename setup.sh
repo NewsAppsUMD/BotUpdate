c73d57506b52fa2db938d901f00fe48780732073
sonicx3087 <106895899+sonicx3087@users.noreply.github.com>
2025-04-06 02:28:59 +0000

    Add GitHub Actions workflow to run botcrime.py weekly

--- setup.sh
+++
@@ -1,9 +0,0 @@
-pip install datasette sqlite_utils
-
-datasette install datasette-codespaces
-
-rm #(database name here)
-
-sqlite-utils insert #(database name) (title of dataabase) (name of csv file).csv --csv
-
-datasette #(database name here)
